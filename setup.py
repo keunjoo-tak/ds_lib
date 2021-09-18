f997207580825ea2c355e2543da8d955507aedad
Takkeunjoo <xkrrmswn@gmail.com>
2021-09-18 09:59:56 +0900

    추가 init

---
+++ setup.py
@@ -0,0 +1,15 @@
+
+from setuptools import setup
+
+setup(
+    name="ds_lib",
+    version="0.0.1",
+    description="keunjoo pip install test",
+    url="https://github.com/keunjoo-tak/ds_lib.git",
+    author="takkeunjoo",
+    author_email="xkrrmswn@gmail.com",
+    license="keunjoo",
+    packages=["ds_lib"],
+    zip_safe=False,
+    install_requires=["numpy==1.18.3"],
+)
